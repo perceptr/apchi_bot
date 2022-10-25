8e0437dde797694d79e215b263196a46d021823d
Konstantin <zaikinke@yandex.ru>
2022-10-25 12:56:04 +0500

    Add new photo parser

--- picture.py
+++ picture.py
@@ -1,18 +1,21 @@
-import requests
-from bs4 import BeautifulSoup as bs
 import random
+from YandexImagesParser.ImageParser import YandexImage
+
+
+def get_picture_links():
+    parser = YandexImage()
+    urls = []
+    for item in parser.search("маникюр без лица"):
+        urls.append(item.preview.url)
+
+    return urls
+
+
+urls = get_picture_links()
 
 
 def get_picture_link():
-    r = requests.get("https://www.google.ru/search?tbm=isch&q=ноготочки")
-    text = r.text
-    soup = bs(text, "html.parser")
-    the_result = {}
-    i = 0
-    for qwerty in soup.find_all('img'):
-        the_result[i] = qwerty.get('src')
-        print(the_result[i])
-        i = i + 1
-        print(i)
-    i = random.randint(1, 19)
-    return the_result[i]
+    return random.choice(urls)
+
+
+print(get_picture_link())
